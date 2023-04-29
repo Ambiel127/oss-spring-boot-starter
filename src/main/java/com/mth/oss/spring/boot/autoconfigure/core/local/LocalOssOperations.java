e75f2e7a0dae73ce7af5cfb064e873e00b8b2628
matianhao <ambiel127@163.com>
2023-04-29 20:39:58 +0800

    local oss 上传操作

--- src/main/java/com/mth/oss/spring/boot/autoconfigure/core/local/LocalOssOperations.java
+++ src/main/java/com/mth/oss/spring/boot/autoconfigure/core/local/LocalOssOperations.java
@@ -1,5 +1,8 @@
 package com.mth.oss.spring.boot.autoconfigure.core.local;
 
+import java.io.File;
+import java.io.InputStream;
+
 /**
  * 本地对象存储服务操作
  *
@@ -8,6 +11,66 @@ package com.mth.oss.spring.boot.autoconfigure.core.local;
  */
 public interface LocalOssOperations {
 
+    // ------------------------------------------------------------
+    // ----------------------- upload 上传 ------------------------
+    // ------------------------------------------------------------
+
+    /**
+     * 通用上传文件
+     * <p>
+     * 默认前缀为 yyyyMMdd 日期文件夹；
+     * 如 file 文件名为 temp.txt，则默认 Object 完整路径为 "yyyyMMdd/temp_yyyyMMddHHmmssSSS.txt"
+     * <p>
+     * 场景：通用业务上传，文件名默认添加时间戳，避免重名导致文件覆盖
+     *
+     * @param file 文件
+     * @return 存储对象相对路径
+     */
+    String upload(File file);
+
+    /**
+     * 通用上传文件，指定 Object 完整路径
+     *
+     * @param file      文件
+     * @param objectKey Object 完整路径，例如 exampleDir/exampleObject.txt
+     * @return 存储对象相对路径
+     */
+    String upload(File file, String objectKey);
+
+    /**
+     * 流式上传
+     *
+     * @param inputStream 流对象
+     *                    文件流：new FileInputStream("D:\\path\\exampleFile.txt");
+     *                    网络流：new URL("https://www.example.com/").openStream();
+     *                    字节数组：new ByteArrayInputStream("Hello OSS".getBytes())
+     * @param objectKey   Object 完整路径，例如 exampleDir/exampleObject.txt
+     * @return 存储对象相对路径
+     */
+    String upload(InputStream inputStream, String objectKey);
+
+    /**
+     * 文件上传并替换同名文件
+     * <p>
+     * 默认路径名为 file 文件名
+     * <p>
+     * 场景：使用手册、说明文件等，需要替换文件但不修改文件路径的场景（路径不变，访问的地址也就不变）
+     *
+     * @param file 文件
+     * @return 存储对象相对路径
+     */
+    String replaceUpload(File file);
+
+    /**
+     * 文件上传并替换同名文件，指定路径名
+     * <p>
+     * 场景：使用手册、说明文件等，需要替换文件但不修改文件路径的场景（路径不变，访问的地址也就不变）
+     *
+     * @param file      文件
+     * @param objectKey Object 完整路径，例如 exampleDir/exampleObject.txt
+     * @return 存储对象相对路径
+     */
+    String replaceUpload(File file, String objectKey);
 
 
 }
--- src/main/java/com/mth/oss/spring/boot/autoconfigure/core/local/LocalOssTemplate.java
+++ src/main/java/com/mth/oss/spring/boot/autoconfigure/core/local/LocalOssTemplate.java
@@ -1,8 +1,20 @@
 package com.mth.oss.spring.boot.autoconfigure.core.local;
 
 import com.mth.oss.spring.boot.autoconfigure.OssProperties;
+import com.mth.oss.spring.boot.autoconfigure.exception.IORuntimeException;
+import com.mth.oss.spring.boot.autoconfigure.handler.DefaultObjectKeyHandler;
+import com.mth.oss.spring.boot.autoconfigure.handler.DefaultOssHandler;
 import com.mth.oss.spring.boot.autoconfigure.handler.OssHandler;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 /**
  * 对象存储服务的 Local 本地实现
@@ -11,13 +23,91 @@ import lombok.RequiredArgsConstructor;
  * @since 1.3
  */
 @RequiredArgsConstructor
-public class LocalOssTemplate implements LocalOssOperations {
+public class LocalOssTemplate implements LocalOssOperations, DefaultObjectKeyHandler {
 
     private final OssProperties ossProperties;
 
     private OssHandler ossHandler;
 
 
+    @Override
+    public String upload(File file) {
+        String pathName = getDefaultObjectKey(file);
+        return upload(file, pathName);
+    }
 
+    @Override
+    public String upload(File file, String objectKey) {
+        try (InputStream source = new FileInputStream(file)) {
+            return upload(source, objectKey);
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+    }
+
+    @Override
+    public String upload(InputStream inputStream, String objectKey) {
+        String completeObjectKey = getCompleteObjectKey(objectKey);
+
+        try {
+            FileUtils.copyToFile(inputStream, new File(completeObjectKey));
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+
+        return objectKey;
+    }
+
+    @Override
+    public String replaceUpload(File file) {
+        String pathName = file.getName();
+        return replaceUpload(file, pathName);
+    }
+
+    @Override
+    public String replaceUpload(File file, String objectKey) {
+        String completeObjectKey = getCompleteObjectKey(objectKey);
+
+        try {
+            Files.deleteIfExists(Paths.get(completeObjectKey));
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+
+        return upload(file, objectKey);
+    }
+
+
+    /**
+     * 获取完整路径
+     *
+     * @param objectKey 路径名，包含文件名
+     * @return 存储对象完整路径
+     */
+    public String getCompleteObjectKey(String objectKey) {
+        String key = ossProperties.getLocalBasePath() + File.separatorChar + objectKey;
+        key = key.replaceAll("\\\\", "/").replaceAll("//", "/");
+        return key;
+    }
+
+    /**
+     * 设置自定义扩展点实现
+     *
+     * @param ossHandler 扩展点实现类
+     */
+    public void setOssHandler(OssHandler ossHandler) {
+        this.ossHandler = Objects.nonNull(ossHandler) ? ossHandler : new DefaultOssHandler();
+    }
+
+    /**
+     * 获取 object key 对应文件
+     *
+     * @param objectKey 路径名，包含文件名
+     * @return 文件对象
+     */
+    private File getObjectFile(String objectKey) {
+        String completeObjectKey = getCompleteObjectKey(objectKey);
+        return new File(completeObjectKey);
+    }
 
 }
